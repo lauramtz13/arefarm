069f2ab9cddc4fa81bce0573bbac916a0e256b48
Laura Martínez Reyes <57735597+lauramtz13@users.noreply.github.com>
2023-05-19 23:18:48 -0600

    Add files via upload

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+function Home() {
+  return (
+    <>
+    <h1 className='text-center'>Farmacias Similares es actualmente la empresa líder en venta y distribución de medicamentos genéricos y productos de salud en México y América Latina.
+
+Inició en el año de 1997, con la finalidad de ofrecer productos y servicios de salud a las comunidades más vulnerables del país.
+
+Actualmente existen más de 6,000 sucursales y franquicias establecidas en todo México y Chile.</h1>
+    
+    </>
+  )
+}
+
+export default Home
